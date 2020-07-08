87988dc49a2f260b5bbae3b3370f8e123d66d814
juandavid716 <jbojato@uninorte.edu.co>
2020-07-08 18:09:04 -0500

    initial interface

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { InputGroup, Input, Button } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    word: "",
+  };
+  getWord() {
+    console.log(this.state.word);
+  }
+  render() {
+    return (
+      <div className="App container">
+        <div className="header mt-5 bg-primary ">
+          <h1 className="p-5">Dictionary Online</h1>
+          <div className=" my-3">
+            <div className="w-75 d-flex">
+              <InputGroup className=" d-flex justify-content-end align-items-center w-100 mr-3 pr-4">
+                <div className="w-50 mb-5">
+                  <Input
+                    type="text"
+                    id="text"
+                    value={this.state.word}
+                    onChange={(e) => {
+                      let findWord = e.target.value;
+                      this.setState({ word: findWord });
+                    }}
+                  />
+                </div>
+              </InputGroup>
+              <div>
+                <Button color="danger" onClick={this.getWord.bind(this)}>
+                  Search
+                </Button>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="d-flex flex-row bg-blue justify-content-around mt-5 bg-success">
+          <div>
+            <h2>Word:</h2>
+          </div>
+          <div>
+            <h2>Meaning</h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
